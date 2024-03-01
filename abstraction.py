2da0413f62d157c92cc58feeb49ce1df4f947b88
Shahboz <sharafzodashahboz@gmail.com>
2024-03-02 00:02:39 +0500

    abstraction concept

---
+++ abstraction.py
@@ -0,0 +1,7 @@
+# Abstraction isn't supported directly in Python. 
+# Calling a magic method, on the other hand, allows for abstraction.
+# If an abstract method is declared in a superclass, subclasses that inherit from the superclass must have their own implementation of the method.
+# A superclass's abstract method will never be called by its subclasses. 
+# But the abstraction aids in the maintenance of a similar structure across all subclasses.
+
+# Code. Coming soon...
