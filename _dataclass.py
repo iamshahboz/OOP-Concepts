0005999719786a695d77345309b0addd42687c76
Shahboz <sharafzodashahboz@gmail.com>
2024-11-08 14:45:33 +0500

    inheritance and immutability

--- _dataclass.py
+++ _dataclass.py
@@ -15,18 +15,43 @@ while using @dataclass decorator, we no longer need to write __init__ function
 from dataclasses import dataclass 
 from typing import Tuple
 
-@dataclass
-class Person():
-    name: str 
-    age: int
-    height: float 
-    email: str 
-    house_coordinates: Tuple
+# @dataclass
+# class Person():
+#     name: str 
+#     age: int
+#     height: float 
+#     email: str 
+#     house_coordinates: Tuple
     
-Person1 = Person(name='Jack', age='13', height=2.10, email='jack@jones.com', house_coordinates=(40.748441, -73.985664))
-print(Person1.house_coordinates)
+# Person1 = Person(name='Jack', age='13', height=2.10, email='jack@jones.com', house_coordinates=(40.748441, -73.985664))
+# print(Person1.house_coordinates)
 
     
 '''
 We could also set a default value
 '''
+
+# we also have immutable dataclasses
+@dataclass(frozen=False)
+class Person():
+    name: str
+    age: int
+    height: float 
+    email: str 
+    
+joe = Person('Joe', 25, 1.85, 'joe@datarequest.io')
+#joe.age = 35 
+
+#print(joe)
+
+'''
+Inheritance with dataclass
+
+The dataclass module also supports inheritance which means we can create a data class that uses te attributes of another data class.
+'''
+@dataclass(order=True)
+class Employee(Person):
+    salary: int
+    department: str 
+    
+print(Employee('Joe', 25, 1.74, 'joedataclass@class.io', 10000, 'Computer Science'))
